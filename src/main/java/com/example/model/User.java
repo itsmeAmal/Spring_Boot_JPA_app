b07dc71bf80ee7521ec761f62337779687dc58ee
Amal <wickramarathna.amal@gmail.com>
2020-07-07 12:20:53 +0530

    implement user entity

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,64 @@
+package com.example.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+		
+	@Id
+	@Column(name="user_id")
+	private int id;
+	
+	@Column(name="user_name")
+	private String userName;
+	
+	@Column(name="user_password")
+	private String userPassword;
+	
+	@Column(name="user_email")
+	private String userEmail;
+	
+	@Column(name="user_status")
+	private int userStatus;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public int getUserStatus() {
+		return userStatus;
+	}
+	public void setUserStatus(int userStatus) {
+		this.userStatus = userStatus;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", userPassword=" + userPassword + ", userEmail="
+				+ userEmail + ", userStatus=" + userStatus + "]";
+	}
+	
+}
