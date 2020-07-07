a08115aef950b15b320c2c023c0451ffa72d711b
Amal <wickramarathna.amal@gmail.com>
2020-07-07 21:06:00 +0530

    user service implement with crud methods

---
+++ src/main/java/com/example/service/userService.java
@@ -0,0 +1,38 @@
+package com.example.service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.springframework.stereotype.Component;
+
+import com.example.model.User;
+
+@Component
+public class userService {
+	
+	public User addUser(User user) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(user);
+		em.getTransaction().commit();		
+		return user;
+	}
+	
+	public User getUserByUserId(int userId) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+		EntityManager em = emf.createEntityManager();
+		User user = em.find(User.class, userId);
+		return user;
+	}
+	
+	public User deleteUserByUserId(int userId) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+		EntityManager em = emf.createEntityManager();
+		return null;
+	}
+	
+	
+
+}
