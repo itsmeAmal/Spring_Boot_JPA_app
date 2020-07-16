49b6eb3be84fc72418a4a67759b642e262f11edc
Amal <wickramarathna.amal@gmail.com>
2020-07-16 06:49:52 +0530

    remove entity manager mechanism and add JPA [spring-boot-starter-data-jpa] through pom.xml

--- src/main/java/com/example/demo/service/userService.java
+++ src/main/java/com/example/demo/service/userService.java
@@ -1,38 +1,19 @@
 package com.example.demo.service;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import org.springframework.stereotype.Component;
+import java.util.List;
 
 import com.example.demo.model.User;
 
-@Component
-public class userService {
+public interface userService {
 	
-	public User addUser(User user) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(user);
-		em.getTransaction().commit();		
-		return user;
-	}
+	public List<User> getAllUsers();
 	
-	public User getUserByUserId(int userId) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
-		EntityManager em = emf.createEntityManager();
-		User user = em.find(User.class, userId);
-		return user;
-	}
+	public User addUser(User user);
 	
-	public User deleteUserByUserId(int userId) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
-		EntityManager em = emf.createEntityManager();
-		User user = em.find(User.class, userId);
-		em.remove(user); 
-		return user;
-	}
+	public User updateUser(User user);
 	
+	public User deleteUser(int userId); 
+	
+	public User getUserByUserId(int userId);
+
 }
