255ce0f942a7772e2a0c9e3db9f92f837e7fadec
Amal <wickramarathna.amal@gmail.com>
2020-07-16 06:46:19 +0530

    add and update user repository and implement user service layer to user service layer

---
+++ src/main/java/com/example/demo/service/impl/userServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.userRepositories;
+import com.example.demo.service.userService;
+
+@Service
+public class userServiceImpl implements userService{
+	
+	@Autowired
+	private userRepositories userRepositories;
+
+	@Override 
+	public List<User> getAllUsers() {
+		List<User> userList = userRepositories.findAll();
+		return userList;
+	}
+
+	@Override
+	public User addUser(User user) {
+		userRepositories.save(user);
+		return user;
+	}
+
+	@Override
+	public User updateUser(User user) {
+		if(user.getId() != 0) {
+			userRepositories.save(user);
+		}
+		return null;
+	}
+
+	@Override
+	public User deleteUser(int userId) {
+		userRepositories.deleteById(userId);
+		User user = userRepositories.getOne(userId);
+		return user;
+	}
+
+	@Override
+	public User getUserByUserId(int userId) {
+		User user = userRepositories.getOne(userId);
+		return user;
+	}
+
+}
